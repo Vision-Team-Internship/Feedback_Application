47df4070c40353fb3fadb1d8ffe4ae550ada462a
NonSinat <sinat.non@student.niptict.edu.kh>
2021-11-07 14:58:50 +0700

    closed #6 Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:feedback_application_flutter/my_splash_screen.dart';
+import 'package:feedback_application_flutter/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MySplashScreen(),
+      home: const LoginScreen(),
     );
   }
 }

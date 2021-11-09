75e70fe43219b052d8dab3abd1eb7d0ffda19488
NonSinat <sinat.non@student.niptict.edu.kh>
2021-11-09 15:16:03 +0700

    delete import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:feedback_application_flutter/my_splash_screen.dart';
-import 'package:feedback_application_flutter/screens/in_processing_screen/in_process_detail_screen.dart';
-import 'package:feedback_application_flutter/screens/in_processing_screen/in_process_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 

c5bd3b1649af6dea7680b541a7e0b60cbf9f4ffb
NonSinat <sinat.non@student.niptict.edu.kh>
2021-11-16 22:02:43 +0700

    closed #25 Detail Message API

--- lib/screens/home/my_home_screen.dart
+++ lib/screens/home/my_home_screen.dart
@@ -18,6 +18,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<CounterData>? _chartData;
+  DateTime? lastPressed;
 
   @override
   void initState() {
@@ -27,158 +28,180 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xffFFFFFF),
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        automaticallyImplyLeading: false,
-        elevation: 0.0,
-        title: Padding(
-          padding: const EdgeInsets.only(left: 10.0),
-          child: RichText(
-            text: TextSpan(
-              text: "Privacy ",
-              style: const TextStyle(
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w600,
-                fontSize: 20,
-                color: Colors.black,
-              ),
-              children: [
-                TextSpan(
-                  text: "Dashboard",
-                  style: TextStyle(
-                    color: ThemeConstant.lightScheme.primary,
-                  ),
+    return WillPopScope(
+      onWillPop: () async {
+        final now = DateTime.now();
+        final maxDuration = Duration(seconds: 2);
+        final isWarning =
+            lastPressed == null || now.difference(lastPressed!) > maxDuration;
+
+        if (isWarning) {
+          lastPressed = DateTime.now();
+          final snackBar = SnackBar(
+            content: Text("Double Tap To Close App"),
+            duration: maxDuration,
+          );
+          ScaffoldMessenger.of(context)
+            ..removeCurrentSnackBar()
+            ..showSnackBar(snackBar);
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+        backgroundColor: const Color(0xffFFFFFF),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          automaticallyImplyLeading: false,
+          elevation: 0.0,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: RichText(
+              text: TextSpan(
+                text: "Privacy ",
+                style: const TextStyle(
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                  color: Colors.black,
                 ),
-              ],
+                children: [
+                  TextSpan(
+                    text: "Dashboard",
+                    style: TextStyle(
+                      color: ThemeConstant.lightScheme.primary,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
+          actions: [
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Stack(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("Notification");
+                    },
+                    child: SvgPicture.asset(
+                      "assets/icons/notification_icons.svg",
+                      width: 26,
+                      cacheColorFilter: true,
+                      height: 26,
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    child: Container(
+                      width: 10,
+                      height: 10,
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        actions: [
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Stack(
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              // ignore: prefer_const_literals_to_create_immutables
               children: [
-                InkWell(
+                const SizedBox(
+                  height: 30,
+                ),
+
+                //  Chart Customize
+                Container(
+                  width: MediaQuery.of(context).size.width - 80,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFBFAFA),
+                    borderRadius: BorderRadius.circular(14),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(2, 2),
+                        color: Colors.black.withOpacity(.25),
+                        blurRadius: 4,
+                      ),
+                      BoxShadow(
+                        offset: const Offset(-1, -1),
+                        color: Colors.black.withOpacity(.25),
+                        blurRadius: 4,
+                      ),
+                    ],
+                  ),
+                  child: const ChartFlutter(),
+                ),
+                const SizedBox(height: 50),
+                FListTile(
                   onTap: () {
                     // ignore: avoid_print
-                    print("Notification");
+                    print("Message");
+                    Get.to(MessageScreen());
                   },
-                  child: SvgPicture.asset(
-                    "assets/icons/notification_icons.svg",
-                    width: 26,
-                    cacheColorFilter: true,
-                    height: 26,
-                  ),
+                  subtitle: 'Recieved: 9+',
+                  title: 'Message',
+                  svgIcon: 'assets/icons/chat_icons.svg',
                 ),
-                Positioned(
-                  right: 0,
-                  child: Container(
-                    width: 10,
-                    height: 10,
-                    decoration: const BoxDecoration(
-                      color: Colors.red,
-                      shape: BoxShape.circle,
-                    ),
-                  ),
+                FListTile(
+                  onTap: () {
+                    // ignore: avoid_print
+                    print("In process");
+                    Get.to(InProcessingScreen());
+                  },
+                  subtitle: 'Recieved: 9+',
+                  title: 'In Processing',
+                  svgIcon: 'assets/icons/inprocess_icons.svg',
+                ),
+                FListTile(
+                  onTap: () {
+                    // ignore: avoid_print
+                    print("History");
+                  },
+                  subtitle: 'Recieved: 9+',
+                  title: 'History',
+                  svgIcon: 'assets/icons/history_icons.svg',
+                ),
+                FListTile(
+                  onTap: () {
+                    // ignore: avoid_print
+                    print("See more");
+                  },
+                  subtitle: 'Body sensors, Calendar and more in settings',
+                  title: 'See other permissions',
+                  svgIcon: 'assets/icons/arrow_down_icons.svg',
+                ),
+
+                FListTile(
+                  onTap: () {
+                    // ignore: avoid_print
+                    print("Setting");
+                  },
+                  subtitle: 'Manage app Account, themes, permissions, etc..',
+                  title: 'Settings',
+                  svgIcon: 'assets/icons/setting_icons.svg',
+                ),
+
+                const SizedBox(
+                  height: 40,
                 ),
               ],
             ),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              const SizedBox(
-                height: 30,
-              ),
-
-              //  Chart Customize
-              Container(
-                width: MediaQuery.of(context).size.width - 80,
-                height: 300,
-                decoration: BoxDecoration(
-                  color: const Color(0xFFFBFAFA),
-                  borderRadius: BorderRadius.circular(14),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset(2, 2),
-                      color: Colors.black.withOpacity(.25),
-                      blurRadius: 4,
-                    ),
-                    BoxShadow(
-                      offset: const Offset(-1, -1),
-                      color: Colors.black.withOpacity(.25),
-                      blurRadius: 4,
-                    ),
-                  ],
-                ),
-                child: const ChartFlutter(),
-              ),
-              const SizedBox(height: 50),
-              FListTile(
-                onTap: () {
-                  // ignore: avoid_print
-                  print("Message");
-                  Get.to(MessageScreen());
-                },
-                subtitle: 'Recieved: 9+',
-                title: 'Message',
-                svgIcon: 'assets/icons/chat_icons.svg',
-              ),
-              FListTile(
-                onTap: () {
-                  // ignore: avoid_print
-                  print("In process");
-                  Get.to(InProcessingScreen());
-                },
-                subtitle: 'Recieved: 9+',
-                title: 'In Processing',
-                svgIcon: 'assets/icons/inprocess_icons.svg',
-              ),
-              FListTile(
-                onTap: () {
-                  // ignore: avoid_print
-                  print("History");
-                },
-                subtitle: 'Recieved: 9+',
-                title: 'History',
-                svgIcon: 'assets/icons/history_icons.svg',
-              ),
-              FListTile(
-                onTap: () {
-                  // ignore: avoid_print
-                  print("See more");
-                },
-                subtitle: 'Body sensors, Calendar and more in settings',
-                title: 'See other permissions',
-                svgIcon: 'assets/icons/arrow_down_icons.svg',
-              ),
-
-              FListTile(
-                onTap: () {
-                  // ignore: avoid_print
-                  print("Setting");
-                },
-                subtitle: 'Manage app Account, themes, permissions, etc..',
-                title: 'Settings',
-                svgIcon: 'assets/icons/setting_icons.svg',
-              ),
-
-              const SizedBox(
-                height: 40,
-              ),
-            ],
           ),
         ),
       ),
